ac5ea9f48c2b94a4ad345eb6ec78087b864b0450
Karol Woźniak <wozniakk@gmail.com>
2020-10-18 23:00:49 +0200

    0.2.0

--- Package.swift
+++ Package.swift
@@ -7,12 +7,12 @@ let package = Package(
         .executable(name: "sciv", targets: ["sciv"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kylef/PathKit", .exact(Version(1, 0, 0))),
-        .package(url: "https://github.com/sciv-img/OSet", .exact(Version(0, 5, 2))),
         .package(url: "https://github.com/sciv-img/Cpcre", .exact(Version(2, 0, 0))),
         .package(url: "https://github.com/sciv-img/Cwebp", .exact(Version(2, 0, 0))),
+        .package(url: "https://github.com/sciv-img/OSet", .exact(Version(0, 6, 0))),
+        .package(url: "https://github.com/kylef/PathKit", .exact(Version(1, 0, 0))),
     ],
     targets: [
-        .target(name: "sciv", dependencies: ["PathKit", "OSet"], path: "Sources"),
+        .target(name: "sciv", dependencies: ["OSet", "PathKit"], path: "Sources"),
     ]
 )
