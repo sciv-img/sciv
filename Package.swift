81c54175ef54a32294c1f8fdd18a8c9a2c164cf4
KenjiTakahashi <wozniakk@gmail.com>
2024-01-03 19:51:09 +0100

    update OSet to latest

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .executable(name: "sciv", targets: ["sciv"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/sciv-img/OSet", .exact(Version(0, 6, 1))),
+        .package(url: "https://github.com/sciv-img/OSet", .exact(Version(0, 7, 0))),
         .package(url: "https://github.com/kylef/PathKit", .exact(Version(1, 0, 1))),
     ],
     targets: [
